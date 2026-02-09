d5af56c5fd2fc19d4c4d45cddf0eadfa2619196e
ZiadRefaie <zaidrar2000@hotmail.com>
2026-02-09 15:58:16 +0200

    GUI BugFixes
    - uploaded the right Gui.py (forgot to upload it lol)
    - added the AR and EN in the Gui.py can be changed manually  (for now)

--- GUI_Code/GUI.py
+++ GUI_Code/GUI.py
@@ -1,16 +1,16 @@
 import sys
 from PySide6.QtWidgets import QApplication
-
-from MainWindow import Main_GUI_Window
+from MainWindow_AR import Widget_AR
+from MainWindow_EN import Widget_EN
 
 
 
 # 1. Create application instance FIRST
 app = QApplication(sys.argv)  
 
-# 2. Create main window
-Main_window = Main_GUI_Window()  
-
+# 2. Create main window ( to switch from EN to AR GUI Just change Widget_EN to Widget_AR)
+Main_window = Widget_AR()  
+Main_window.setFixedSize(1024, 768)
 
 # 3. Show window
 Main_window.show()    
